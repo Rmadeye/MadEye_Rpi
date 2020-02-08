67f6f5bd36c142f4305890501176ef0b8af3cb18
Rmadeye <rafmadaj@gmail.com>
2020-02-08 11:49:16 +0100

    Update

--- src/driver.py
+++ src/driver.py
@@ -5,28 +5,27 @@ Python driver for raspberry temperature sensor
 
 import datetime
 import smtplib
-from src import temp as t, alarm_socket as alarm
+from src import temp as t
 #import flowmeter as f
 import RPi.GPIO as GPIO
 import time
 
 class Driver:
     def __init__(self):
+        print("Initializing")
         self.temp_driver = t.TempControl()
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(18, GPIO.OUT) # Buzzer online
-        GPIO.setup(21, GPIO.OUT) # Socket online
-        GPIO.setup(20, GPIO.OUT) # Diode online
+        #GPIO.setup(18, GPIO.OUT) # Diode online
+        GPIO.setup(21, GPIO.OUT) # Buzzer online
         GPIO.setup(19, GPIO.OUT) # Check temp diode online
-        GPIO.output(20,1)
-        GPIO.output(18,0)
+        GPIO.output(21,0)
         time.sleep(0.2)
         GPIO.output(18,1)
 
 
     def collect_data(self):
         log=open("log.txt","a+")
-        current_temp = int(self.temp_driver.read_temperature())
+        current_temp = int(self.temp_driver.check_temperature())
     #    outflow=f.collect_flow()
         time=datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         print(time)
@@ -38,30 +37,30 @@ class Driver:
         if temp <30:
             GPIO.output(19, 1)
 
-        elif temp>30:
-            GPIO.output(19, 0)
+        # elif temp>30:
+        #     GPIO.output(19, 0)
 
 
         elif temp >40:
             print("WARNING! Temperature too high!")
-            GPIO.output(18, 0)  # Buzzer on - ALARM LOUD
+            GPIO.output(21, 0)  # Buzzer on - ALARM LOUD
             time.sleep(0.5)
-            GPIO.output(18, 1)  # Buzzer on - ALARM LOUD
+            GPIO.output(21, 1)  # Buzzer on - ALARM LOUD
             time.sleep(0.5)
-            GPIO.output(18, 0)  # Buzzer on - ALARM LOUD
+            GPIO.output(21, 0)  # Buzzer on - ALARM LOUD
             time.sleep(0.5)
-            GPIO.output(18, 1)  # Buzzer on - ALARM LOUD
+            GPIO.output(21, 1)  # Buzzer on - ALARM LOUD
             time.sleep(0.5)
-            GPIO.output(18, 0)  # Buzzer on - ALARM LOUD
+            GPIO.output(21, 0)  # Buzzer on - ALARM LOUD
             time.sleep(0.5)
-            GPIO.output(18, 1)  # Buzzer on - ALARM LOUD
+            GPIO.output(21, 1)  # Buzzer on - ALARM LOUD
 
         elif temp>55:
-            GPIO.output(18, 0)  # Buzzer on - ALARM LOUD
+            GPIO.output(21, 0)  # Buzzer on - ALARM LOUD
             time.sleep(5)
-            GPIO.output(18, 1)  # Buzzer on - ALARM LOUD
-            time.sle(0.1)
-            GPIO.output(18, 0)  # Buzzer on - ALARM LOUD
+            GPIO.output(21, 1)  # Buzzer on - ALARM LOUD
+            time.sleep(0.1)
+            GPIO.output(21, 0)  # Buzzer on - ALARM LOUD
         
 
 
--- src/temp.py
+++ src/temp.py
@@ -1,16 +1,52 @@
-import w1thermsensor
+import w1thermsensor, time
+import RPi.GPIO as GPIO
 
 
 class TempControl:
     def __init__(self):
-        pass
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
 
 
-    def read_temperature(self):
+    def check_temperature(self):
 
         sensor = w1thermsensor.W1ThermSensor()
         temp = sensor.get_temperature()
-        print(str(temp))
-        return temp
+
+        if temp < 35:
+
+            pass
+
+        if temp > 35 and temp <50:
+            GPIO.setup(21, GPIO.OUT)
+            time.sleep(0.5)
+            GPIO.output(21, GPIO.HIGH)
+            time.sleep(0.5)
+            GPIO.setup(21, GPIO.OUT)
+            time.sleep(0.5)
+            GPIO.output(21, GPIO.HIGH)
+            time.sleep(0.5)
+            GPIO.setup(21, GPIO.OUT)
+            time.sleep(0.5)
+            GPIO.output(21, GPIO.HIGH)
+
+        if temp > 50:
+            GPIO.setup(21, GPIO.OUT)
+            time.sleep(3)
+            GPIO.output(21, GPIO.HIGH)
+            time.sleep(0.2)
+            GPIO.setup(21, GPIO.OUT)
+            time.sleep(0.2)
+            GPIO.output(21, GPIO.HIGH)
+            time.sleep(0.2)
+            GPIO.setup(21, GPIO.OUT)
+            time.sleep(0.2)
+            GPIO.output(21, GPIO.HIGH)
+
+
+
+
+
+
 
 
