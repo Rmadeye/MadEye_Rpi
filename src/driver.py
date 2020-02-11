b5889cbd71b2034bc913550a2f9ba2b7ac26fb91
Rmadeye <rafmadaj@gmail.com>
2020-02-11 21:05:04 +0000

    Check

--- src/driver.py
+++ src/driver.py
@@ -17,20 +17,19 @@ class Driver:
         GPIO.setmode(GPIO.BCM)
         #GPIO.setup(18, GPIO.OUT) # Diode online
         GPIO.setup(21, GPIO.OUT) # Buzzer online
-        GPIO.setup(19, GPIO.OUT) # W1 online
+        #GPIO.setup(19, GPIO.OUT) # W1 online
         GPIO.setup(26, GPIO.OUT) # Check temp diode online
         GPIO.output(21,0)
 
         time.sleep(0.2)
         GPIO.output(21,1)
-        GPIO.output(4,1)
 
 
 
     def collect_data(self):
         log=open("log.txt","a+")
         current_temp = int(self.temp_driver.check_temperature())
-    #    outflow=f.collect_flow()
+        print(float(current.temp))
         time=datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         log.write(time +" Temperature: "+ str(current_temp)+"\xb0C"+"\n")
         log.close()
@@ -38,13 +37,13 @@ class Driver:
 
     def warnings(self,temp):#,outflow):
         if temp <30:
-            GPIO.output(19, 1)
+            GPIO.output(26, 1)
 
-        # elif temp>30:
-        #     GPIO.output(19, 0)
+        elif temp>30:
+            GPIO.output(26, 0)
 
 
-        elif temp >40:
+        elif temp >31:
             print("WARNING! Temperature too high!")
             GPIO.output(21, 0)  # Buzzer on - ALARM LOUD
             time.sleep(0.5)
